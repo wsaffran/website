b197b3f1d946f7691479b580850b14650a479b12
github username <github email address>
2019-08-27 22:11:26 -0400

    graphql setup

--- server/app.js
+++ server/app.js
@@ -1,5 +1,13 @@
 const express = require('express')
+const graphqlHttp = require('express-graphql')
 
 const app = express()
 
-app.listen(4000)
+app.use('/graphql', graphqlHttp({
+  schema: null,
+  graphiql: true
+}))
+
+app.listen(4000, () => {
+  console.log('now listening on port 4000')
+})
