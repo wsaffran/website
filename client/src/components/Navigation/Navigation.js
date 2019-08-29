181fd9d90c19543c4a711948a100765a2237d8bb
github username <github email address>
2019-08-28 22:36:35 -0400

    front end setup

---
+++ client/src/components/Navigation/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+import './Navigation.css'
+
+const Navigation = props => {
+  return(
+    <header className="navigation">
+      <div className="navigation__logo">
+        <h1>Will Saffran</h1>
+      </div>
+    </header>
+  )
+}
+
+export default Navigation
